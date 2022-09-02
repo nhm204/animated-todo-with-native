fd9e07ad718da5b3aab5ac36eed14f9447eba310
nhm204 <19521862@gm.uit.edu.vn>
2022-09-02 23:56:56 +0700

    integrated react-navigation and drawer

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import AppContainer from './src/components/app-container';
-import MainScreen from './src/screens/main-screen';
+import Navigator from './src/';
 
 
 export default function App () {
   return (
     <AppContainer>
-      <MainScreen />
+      <Navigator />
     </AppContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
