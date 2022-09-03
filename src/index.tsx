cb8ee25085e245e3fec91bb29d035f03a5a94f08
nhm204 <19521862@gm.uit.edu.vn>
2022-09-04 00:16:08 +0700

    add sidebar

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import MainScreen from './screens/main-screen';
 import AboutScreen from './screens/about-screen';
+import Sidebar from './components/sidebar';
 
 
 const Drawer = createDrawerNavigator();
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <Drawer.Navigator 
       initialRouteName='Main' 
+      drawerContent={props => <Sidebar {...props} />}
       screenOptions={{
         headerShown: false,
         drawerType: 'back',
